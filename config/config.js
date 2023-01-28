69b6d880477f636b5d9a4ab613b27ca4c3060d58
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2023-01-28 11:07:50 -0500

    deploy-test3

--- config/config.js
+++ config/config.js
@@ -20,17 +20,17 @@ module.exports =
     
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DBHOST,
+    "password": process.env.DBPASSWORD,
+    "database": process.env.DBNAME,
+    "host": process.env.DBHOST,
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DBHOST,
+    "password": process.env.DBPASSWORD,
+    "database": process.env.DBNAME,
+    "host": process.env.DBHOST,
     "dialect": "postgres"
   }
 }
